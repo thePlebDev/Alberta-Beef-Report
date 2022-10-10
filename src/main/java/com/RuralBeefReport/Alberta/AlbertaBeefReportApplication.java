80bb18f7d8865bb84ca5202b4b328e86ad69a739
Tristan Elliott <>
2022-10-09 21:20:00 -0300

    initial commit

---
+++ src/main/java/com/RuralBeefReport/Alberta/AlbertaBeefReportApplication.java
@@ -0,0 +1,13 @@
+package com.RuralBeefReport.Alberta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbertaBeefReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbertaBeefReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RuralBeefReport/Alberta/AlbertaBeefReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RuralBeefReport.Alberta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbertaBeefReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
